64d9f7967127b9154c9e3382a4575f957c67d120
AliakseiBarysau <undergou@mail.ru>
2020-11-18 12:57:17 +0300

    16

--- basic/mail/resetPassword.php
+++ basic/mail/resetPassword.php
@@ -5,10 +5,12 @@
 
 	echo "Hello, ".Html::encode($user->username)."!";
 	echo Html::a('Enter this link to reset password.',
-		Yii::$app->urlManager->createAbsoluteUrl(
-		[
-			'/usAuth/default/password-reseting',
-			'key' => $user->resetKey,
-		]
-	));
+//		Yii::$app->urlManager->createAbsoluteUrl(
+//		[
+//			'/usAuth/default/password-reseting',
+//			'key' => $user->resetKey,
+//		]
+//	)
+        'http://basic/usAuth/default/password-reseting?key='. $user->resetKey
+);
 ?>
