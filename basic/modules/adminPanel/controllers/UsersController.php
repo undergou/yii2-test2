88da0731e941e1767818ac38af53119b1ceeb93c
AliakseiBarysau <undergou@mail.ru>
2020-11-19 11:01:57 +0300

    36

--- basic/modules/adminPanel/controllers/UsersController.php
+++ basic/modules/adminPanel/controllers/UsersController.php
@@ -128,11 +128,11 @@ class UsersController extends Controller
 //            }
 
             if($model->password === $user->password){
-//	            $model->save(false);
+	            $model->save(false);
 	            return $this->redirect(['view', 'id' => $model->id]);
 	        } else{
-//	        	$model->setPassword($model->password);
-//	        	$model->save(false);
+	        	$model->setPassword($model->password);
+	        	$model->save(false);
 	        	return $this->redirect(['view', 'id' => $model->id]);
 	        }
         }
